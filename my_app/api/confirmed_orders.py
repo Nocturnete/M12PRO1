18051e98c3c502267103214162f4b77094d8b76e
CristianMartinezGuerrero <crmagu@fp.insjoaquimmir.cat>
2024-02-07 11:56:42 +0100

    auth token

--- my_app/api/confirmed_orders.py
+++ my_app/api/confirmed_orders.py
@@ -8,16 +8,16 @@ from flask import current_app
 @api_bp.route('/orders/<int:id>/confirmed', methods=['POST'])
 @token_auth.login_required
 def confirm_order(id):
-    
-    existing_confirmation = ConfirmedOrder.query.filter_by(order_id=id).first()
-    if existing_confirmation:
-        return json_response(existing_confirmation.to_dict())  
-
     order = Order.get(id)
     product = Product.get(order.product_id)
     user_current = token_auth.current_user().id
+    current_app.logger.debug(product.seller_id)
+    current_app.logger.debug(user_current)
     if product.seller_id == user_current:
         if order:
+            existing_confirmation = ConfirmedOrder.query.filter_by(order_id=id).first()
+            if existing_confirmation:
+                return not_found("ConfirmedOrder already exist")
             try:
                 new_confirm = ConfirmedOrder()
                 new_confirm.order_id=id
@@ -32,15 +32,20 @@ def confirm_order(id):
         return not_found("Permission denied")
     
 @api_bp.route('/orders/<int:id>/confirmed', methods=['DELETE'])
+@token_auth.login_required
 def confirm_order_delete(id):
-
-    order = ConfirmedOrder.get(id)
-
-    if order:
-        try:
-            ConfirmedOrder.delete(order)
-            return json_response({"message": "ConfirmedOrder deleted successfully"})
-        except Exception as e:
-            return bad_request(str(e))
+    orderConfirmed = ConfirmedOrder.get(id)
+    order = Order.get(orderConfirmed.order_id)
+    product = Product.get(order.product_id)
+    user_current = token_auth.current_user().id
+    if product.seller_id == user_current:
+        if orderConfirmed:
+            try:
+                ConfirmedOrder.delete(orderConfirmed)
+                return json_response({"message": "ConfirmedOrder deleted successfully"})
+            except Exception as e:
+                return bad_request(str(e))
+        else:
+            return not_found("ConfirmedOrder not found")
     else:
-        return not_found("ConfirmedOrder not found")
+        return not_found("Permission denied")
